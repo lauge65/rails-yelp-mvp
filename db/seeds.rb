eb826e869c9d19e93d9183ecdc2994f784f458c3
Ludovic AUGE <auge.ludovic@gmail.com>
2026-02-24 13:02:00 +0100

    Rails_yelp_mvp done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants"
+Restaurant.create!(name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "1111111", category: "chinese")
+puts "Created Dishoom"
+Restaurant.create!(name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "2222222", category: "italian")
+puts "Created Pizza East"
+Restaurant.create!(name: "La Table Ronde", address: "Argeles Gazost", phone_number: "333333333", category: "french")
+puts "Created La Table Ronde"
+Restaurant.create!(name: "Isubu", address: "San Francisco", phone_number: "444444444", category: "japanese")
+puts "Created Isubu"
+Restaurant.create!(name: "Au petit restaurant", address: "Bruxelles", phone_number: "55555555", category: "belgian")
+puts "Created Au petit restaurant"
